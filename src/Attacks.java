51b5f5cc5ef76a83b072eb87cb995ae34997f985
kareemfelfel <k.a.ahmed@eagle.clarion.edu>
2020-03-14 00:22:56 -0400

    Updated Changes

--- src/Attacks.java
+++ src/Attacks.java
@@ -1,5 +1,23 @@
 public class Attacks {
+    //Field variables
     private String Name;
     private int PowerPoints;
+
+    //GETTERS AND SETTERS
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getPowerPoints() {
+        return PowerPoints;
+    }
+
+    public void setPowerPoints(int powerPoints) {
+        PowerPoints = powerPoints;
+    }
 
 }
