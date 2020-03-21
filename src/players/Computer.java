7a9dfbf1d73fb7d45fd1c64d7d2fd967d0cbc91d
kareemfelfel <k.a.ahmed@eagle.clarion.edu>
2020-03-20 21:25:57 -0400

    Updated Changes

--- src/players/Computer.java
+++ src/players/Computer.java
@@ -28,12 +28,18 @@ public class Computer extends Player
     public String Reply(Player User)
     {
         String Statement ="";
+        // If my current pokemon's Hit Points is less than or equal to 10
+        if(getCurrentPokemon().getHitPoints()<= 25 && getCurrentPokemon().getHitPoints()!= 0)
+        {
+            Switch();
+            Statement = "Computer switched his pokemon to "+ getCurrentPokemon().getName() + ". ";
+        }
         //if a booster is found
         if(BoosterIsFound() && getCurrentPokemon().getHitPoints()<100)
         {
             Boosters boosterFound = getBooster();
             boosterFound.use(this);
-            Statement = getCurrentPokemon().getName()+ " Found a " + boosterFound.getName() + " and maxed its energy.";
+            Statement += getCurrentPokemon().getName()+ " Found a " + boosterFound.getName() + " and maxed its energy. ";
 
         }
         //If an Item is found
@@ -45,12 +51,6 @@ public class Computer extends Player
             Statement += getCurrentPokemon().getName()+ " Found a " + weaponFound.getName() + " and used it on you! ";
 
         }
-        // If my current pokemon's Hit Points is less than or equal to 10
-        else if(getCurrentPokemon().getHitPoints()<= 10 && getCurrentPokemon().getHitPoints()!= 0)
-        {
-            Switch();
-            Statement += "Computer switched his pokemon to "+ getCurrentPokemon().getName();
-        }
         // if anything else
         else
         {
@@ -58,22 +58,22 @@ public class Computer extends Player
             if(!getCurrentPokemon().getAttacksList().get(0).Missed()) {
                 boolean found = false;
                 Random random = new Random();
-                Attacks attack = getCurrentPokemon().getAttacksList().get(random.nextInt(BoostersList.size()));
+                Attacks attack = getCurrentPokemon().getAttacksList().get(random.nextInt(getCurrentPokemon().getAttacksList().size()));
                 if(attack.getPowerPoints()!=0)
                     found = true;
                 int counter =0;
                 //if the attack's powerpoints !=0
                 while(!found)
                 {
-                    random = new Random();
-                    attack = getCurrentPokemon().getAttacksList().get(random.nextInt(BoostersList.size()));
+                    Random random2 = new Random();
+                    attack = getCurrentPokemon().getAttacksList().get(random.nextInt(getCurrentPokemon().getAttacksList().size()));
                     if(attack.getPowerPoints()!=0)
                         found = true;
                     else if(counter ==50)
                     {
                         //Switch pokemons if we have not found any available attacks to play
                         Switch();
-                        Statement += "Computer switched his pokemon to "+ getCurrentPokemon().getName();
+                        Statement += "Computer switched his pokemon to "+ getCurrentPokemon().getName() + ". ";
                         break;
                     }
                     counter +=1;
@@ -84,11 +84,11 @@ public class Computer extends Player
                     attack.Use(User.getCurrentPokemon());
                     //decrease the attack's powerpoints
                     attack.setPowerPoints();
-                    Statement += getCurrentPokemon().getName() + " used " + attack.getName() + " on you!";
+                    Statement += getCurrentPokemon().getName() + " used " + attack.getName() + " on you! ";
                 }
             }
             else
-                Statement += getCurrentPokemon().getName() + "missed its attack on you!";
+                Statement += getCurrentPokemon().getName() + " missed its attack on you! ";
         }
         return Statement;
     }
